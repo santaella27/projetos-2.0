ad318d1be1d2179ffdbf819ebad7abab9886ebd8
Sebastian Santaella <141974972+santaella27@users.noreply.github.com>
2024-04-21 11:03:42 -0300

    cpf.py

--- cpf.py
+++ cpf.py
@@ -1,9 +1,8 @@
 def validar_cpf(cpf):
-    cpf = ''.join(filter(str.isdigit, cpf))  # Remove caracteres não numéricos do CPF
-    if len(cpf) != 11:  # Verifica se o CPF tem 11 dígitos
+    cpf = ''.join(filter(str.isdigit, cpf))  
+    if len(cpf) != 11: 
         return False
-    
-    # Calcula o primeiro dígito verificador
+        
     soma = 0
     for i in range(9):
         soma += int(cpf[i]) * (10 - i)
@@ -11,11 +10,9 @@ def validar_cpf(cpf):
     if primeiro_digito == 10:
         primeiro_digito = 0
     
-    # Verifica se o primeiro dígito verificador está correto
     if primeiro_digito != int(cpf[9]):
         return False
-    
-    # Calcula o segundo dígito verificador
+
     soma = 0
     for i in range(10):
         soma += int(cpf[i]) * (11 - i)
@@ -23,13 +20,11 @@ def validar_cpf(cpf):
     if segundo_digito == 10:
         segundo_digito = 0
     
-    # Verifica se o segundo dígito verificador está correto
     if segundo_digito != int(cpf[10]):
         return False
     
     return True
 
-# Exemplo de uso:
 cpf_usuario = input("Digite o CPF (apenas números): ")
 if validar_cpf(cpf_usuario):
     print("CPF Válido.")
